53acd82439245fe9fa8c51e2eec2f05638ae40b7
skyylord0 <amine.akhlafa1@gmail.com>
2026-01-29 16:22:49 +0100

    Added caption feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
 import { useState } from 'react'
 import './App.css'
 import SelectType from './SelectType'
+import AddText from './AddText';
 
 function App() {
   const [type, setType] = useState("image"); // default = static image
+  const [text, setText] = useState("");       // validated caption
+  const [inputText, setInputText] = useState(""); // live typing
 
   //Define the image URL path 
   function imageURL() {
     let base = "https://cataas.com/cat";
 
+    //If GIF type is selected
     if (type === "gif") {
       base += "/gif";
     }
 
+    // Add code to put tags
+
+    //If text is added
+    //// A condition vrifiying if text is empty
+    if (text !== "") {
+      const encodedText = encodeURIComponent(text)
+      base += "/says/" + encodedText;
+    }
+
+    console.log(base)
     return base;
   }
 
@@ -22,6 +36,11 @@ function App() {
 
       <div className="toolbox">
         <SelectType onTypeChange={setType} />
+        <AddText
+          inputText={inputText}
+          onInputChange={setInputText}
+          onValidate={() => setText(inputText)}
+        />
       </div>
 
       <div className="card">
